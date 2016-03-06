f9421f738c4ddca38ab47c9fa335906179360955
zy-AK1900 <447490313@qq.com>
2016-03-06 16:36:06 +0800

    四则运算生成程序
    随机生成30个小学的四则运算题

---
+++ main.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+
+void main()
+{
+	int a, b, i, f, t;
+	for (i = 0; i < 30; i++)
+	{
+
+		a = rand() % 100;
+		b = rand() % 100;
+		if (a < b)
+		{
+			t = a;
+			a = b;
+			b = t;
+		}
+		cout << a;
+
+		f = rand() % 4;
+		switch (f)
+		{
+		case 0:cout << '+'; break;
+		case 1:cout << '-'; break;
+		case 2:cout << '*'; break;
+		case 3:cout << '/'; break;
+		}
+		if (f % 4 == 3)
+			b++;
+		cout << b << '=' << endl;
+	}
+}
